fa16871bc19b1c6f36ac9e3bbcdbf022d799c9fe
lxz <107622024@qq.com>
2019-01-09 11:20:25 +0800

    modify user

--- demo/User.cs
+++ demo/User.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace demo
 {
+    /// <summary>
+    /// 用户类
+    /// </summary>
     public class User
     {
+        public string name { get; set; }
+        public int age { get; set; }
     }
 }
